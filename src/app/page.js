125531672e9632c13992aa58b2e2e5557eb6b595
Michael Hird <mhird@smartergridsolutions.com>
2024-11-06 10:18:24 +0000

    Fixing home page banner styles

--- src/app/page.js
+++ src/app/page.js
@@ -10,13 +10,13 @@ export default function Home() {
       <section className={styles.baseSection} id="home">
 
         <div className={styles.sectionTitleContainer}>
-          <div className={styles.sectionTitleItem}>
+          <div className={`${styles.sectionTitleItem} ${styles.sectionTitleItemWidth}`}>
             <div className={styles.imageContainer}>
               <Image
                 src={bandLogo} 
                 fill={true}
                 layout="fill"
-                objectFit="contain"
+                objectFit="cover"
               />     
             </div>
           </div>
@@ -42,7 +42,7 @@ export default function Home() {
       </section>
       <section className={styles.baseSection} id="music">
         <div className={`${styles.sectionTitleContainer} ${styles.musicTitleBackground}`}>
-          <h1>Music Section Title</h1>
+          <h1>Our Music</h1>
         </div>
         <div className={styles.sectionMainContent}>
           <h1>Music section content</h1>
@@ -50,7 +50,7 @@ export default function Home() {
       </section>
       <section className={styles.baseSection} id="about">
         <div className={`${styles.sectionTitleContainer} ${styles.aboutTitleBackground}`}>
-          <h1>About Section Title</h1>
+          <h1>Our Story</h1>
         </div>
         <div className={styles.sectionMainContent}>
           <h1>About Section Content</h1>
@@ -58,7 +58,7 @@ export default function Home() {
       </section>
       <section className={styles.baseSection} id="tour">
       <div className={`${styles.sectionTitleContainer} ${styles.tourTitleBackground}`}>
-          <h1>Tour Section Title</h1>
+          <h1>Live Shows</h1>
         </div>
         <div className={styles.sectionMainContent}>
           <h1>Tour Section Content</h1>
@@ -66,7 +66,7 @@ export default function Home() {
       </section>
       <section className={styles.baseSection} id="contact">
       <div className={`${styles.sectionTitleContainer} ${styles.contactTitleBackground}`}>
-          <h1>Contact Section Title</h1>
+          <h1>Get In Touch</h1>
         </div>
         <div className={styles.sectionMainContent}>
           <h1>Contact Section Content</h1>
