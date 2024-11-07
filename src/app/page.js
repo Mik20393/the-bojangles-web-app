ac6205c32682a6901ee772409e3e41495896664f
Michael Hird <mhird@smartergridsolutions.com>
2024-11-07 16:17:53 +0000

    Changing layout

--- src/app/page.js
+++ src/app/page.js
@@ -7,74 +7,92 @@ import heroImage from "@/public/heroImage.jpg"
 export default function Home() {
   return (
     <div className={styles.main}>
+
       <section className={styles.baseSection} id="home">
 
-        <div className={styles.sectionTitleContainer}>
-          <div className={styles.logoImageContainer}>
-            <Image
-              src={bandLogo} 
-              fill={true}
-              layout="fill"
-              objectFit="fill"
-            />     
+        <div className={styles.largeContentContainer}>
+          <div className={styles.sectionTitleContainer}>
+            <div className={styles.logoImageContainer}>
+              <Image
+                src={bandLogo} 
+                fill={true}
+                layout="fill"
+                objectFit="fill"
+              />     
+            </div>
+            <div className={styles.navMenuContainer}>
+              <h1>NAV</h1>
+            </div>
           </div>
-          <div className={styles.navMenuContainer}>
-            <h1>NAV</h1>
+          <div className={styles.sectionMainContent}>
+            <div className={styles.heroImageContainer}>
+              <Image 
+                src={heroImage}
+                fill={true}
+                layout="fill"
+                objectFit="cover"
+              />
+            </div>
           </div>
         </div>
-
-        <div className={styles.sectionMainContent}>
-          <div className={styles.heroImageContainer}>
-            <Image 
-              src={heroImage}
-              fill={true}
-              layout="fill"
-              objectFit="cover"
-            />
-          </div>
-        </div>
-
+        
       </section>
-      <section className={styles.baseSection} id="music">
-        <div className={`${styles.sectionTitleContainer} ${styles.musicTitleBackground}`}>
-          <div className={styles.sectionTitle}>
-            <h1>Our Music</h1>
-          </div>
-        </div>
-        <div className={styles.sectionMainContent}>
-          <h1>Music section content</h1>
-        </div>
-      </section>
+
       <section className={styles.baseSection} id="about">
-        <div className={`${styles.sectionTitleContainer} ${styles.aboutTitleBackground}`}>
-          <div className={styles.sectionTitle}>
-            <h1>Our Story</h1>
+
+        <div className={styles.smallContentContainer}>
+          <div className={`${styles.sectionTitleContainer} ${styles.musicTitleBackground}`}>
+            <div className={styles.sectionTitle}>
+              <h1>Our Music</h1>
+            </div>
+          </div>
+          <div className={styles.sectionMainContent}>
+            <h1>Music section content</h1>
           </div>
         </div>
-        <div className={styles.sectionMainContent}>
-          <h1>About Section Content</h1>
+        <div className={styles.smallContentContainer}>
+          <div className={`${styles.sectionTitleContainer} ${styles.aboutTitleBackground}`}>
+            <div className={styles.sectionTitle}>
+              <h1>Our Story</h1>
+            </div>
+          </div>
+          <div className={styles.sectionMainContent}>
+            <h1>About Section Content</h1>
+          </div>
         </div>
+
       </section>
+
       <section className={styles.baseSection} id="tour">
-        <div className={`${styles.sectionTitleContainer} ${styles.tourTitleBackground}`}>
-          <div className={styles.sectionTitle}>
-            <h1>Live Shows</h1>
+
+        <div className={styles.largeContentContainer}>
+          <div className={`${styles.sectionTitleContainer} ${styles.tourTitleBackground}`}>
+            <div className={styles.sectionTitle}>
+              <h1>Live Shows</h1>
+            </div>
+          </div>
+          <div className={styles.sectionMainContent}>
+            <h1>Tour Section Content</h1>
           </div>
         </div>
-        <div className={styles.sectionMainContent}>
-          <h1>Tour Section Content</h1>
-        </div>
+        
       </section>
+
       <section className={styles.baseSection} id="contact">
-        <div className={`${styles.sectionTitleContainer} ${styles.contactTitleBackground}`}>
-          <div className={styles.sectionTitle}>
-            <h1>Get In Touch</h1>
+
+        <div className={largeContentContainer}>
+          <div className={`${styles.sectionTitleContainer} ${styles.contactTitleBackground}`}>
+            <div className={styles.sectionTitle}>
+              <h1>Get In Touch</h1>
+            </div>
+          </div>
+          <div className={styles.sectionMainContent}>
+            <h1>Contact Section Content</h1>
           </div>
         </div>
-        <div className={styles.sectionMainContent}>
-          <h1>Contact Section Content</h1>
-        </div>
+        
       </section>
+
     </div>
   );
 }
