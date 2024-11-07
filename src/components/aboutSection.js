fa0fce2110277d1a001c363b2a9e9c8075c2a80c
Michael Hird <mhird@smartergridsolutions.com>
2024-11-07 16:47:47 +0000

    Updating the paragraph in about section

--- src/components/aboutSection.js
+++ src/components/aboutSection.js
@@ -3,17 +3,20 @@ import styles from "./about.module.css"
 export default function AboutSection() {
     return (
         <div className={styles.aboutSectionContainer}>
-            <p>We are The Bojangles, an alternative hip-hop live band</p>
-            <p>from Glasgow, formed in 2023. We love experimenting with</p>
-            <p>jazzy, funky feel-good rhythyms, our lyrics range from</p>
-            <p>playful to introspective.</p>
-            <p>The band came together through a serious of fortuitous</p>
-            <p>encounters at a local pub, where musicians from different</p>
-            <p>backgrounds and musical influences connected, all sharing</p>
-            <p>a passion and desire to be a part of something exciting and</p>
-            <p>unique. Our music features Portuguese lyrics, Latin</p>
-            <p>melodies, boom-bap beats, funky guitar riffs, brass</p>
-            <p>solos - you name it, all played live!</p>
+            <p>
+                We are The Bojangles, an alternative hip-hop live band <br />
+                from Glasgow, formed in 2023. We love experimenting with <br />
+                jazzy, funky feel-good rhythyms, our lyrics range from <br />
+                playful to introspective. <br />
+                <br />
+                The band came together through a serious of fortuitous <br />
+                encounters at a local pub, where musicians from different <br />
+                backgrounds and musical influences connected, all sharing <br />
+                a passion and desire to be a part of something exciting and <br />
+                unique. Our music features Portuguese lyrics, Latin <br />
+                melodies, boom-bap beats, funky guitar riffs, brass <br />
+                solos - you name it, all played live!            
+            </p>
         </div>
     )
 }
