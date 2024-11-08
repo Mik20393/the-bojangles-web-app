646b16c85c07e4ae65d0318c54bb3faa859bb544
Michael Hird <mhird@smartergridsolutions.com>
2024-11-08 12:10:27 +0000

    Update poster styling

--- src/components/tourSection.js
+++ src/components/tourSection.js
@@ -19,6 +19,7 @@ export default function TourSection() {
                     width={16}
                     height={9}
                     layout="responsive"
+                    objectFit="contain"
                 />
                 
             </div>
