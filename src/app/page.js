34136fb61dc6d98a73ed28b0f8ea96f291ea71ac
Michael Hird <mhird@smartergridsolutions.com>
2024-11-14 09:51:11 +0000

    Adding social media icons

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,10 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
+import { FaYoutube } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa";
+
 import HomeSectionContent from "@/components/homeSection";
 import AboutSectionContent from "@/components/aboutSection";
 import MusicSectionContent from "@/components/musicSection";
@@ -27,7 +31,9 @@ export default function Home() {
               />     
             </div>
             <div className={styles.socialsContainer}>
-              <h1>SOCIALS</h1>
+              <h1><a href=""><FaXTwitter /></a></h1>
+              <h1><a href=""><FaInstagram /></a></h1>
+              <h1><a href=""><FaYoutube /></a></h1>
             </div>
           </div>
           <div className={styles.sectionMainContent}>
