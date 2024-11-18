6d5b92debb7bdc9006344f9be4fa1bc20196601d
Michael Hird <mhird@smartergridsolutions.com>
2024-11-18 10:07:13 +0000

    Styling contact form

--- src/components/contactSection.js
+++ src/components/contactSection.js
@@ -6,17 +6,19 @@ export default function ContactSection() {
             <div className={styles.contactSectionContainer}>
                 <div className={styles.contactSectionTextContainer}>
                     <p>
-                        Hey there you wanna book us to play or just tell <br/>
-                        us something please fill the form below or write to us at <br/>
+                        Hey there you wanna book us to play or just tell 
+                        us something please fill the form below or write to us at 
                         thebojanglesglasgow@gmail.com
                     </p>
                 </div>
                 <div className={styles.contactFormContainer}>
                     <form className={styles.contactForm}>
-                        <input type="text" name="name" placeholder="Name" required/>
-                        <input type="email" name="email" placeholder="Email" required/>
-                        <textarea name="message" placeholder="Message" required></textarea>
-                        <button type="submit">Submit</button>
+                        <input type="text" className={styles.formInputControl} name="name" placeholder="Name" required/>
+                        <input type="email" className={styles.formInputControl} name="email" placeholder="Email" required/>
+                        <textarea name="message" className={styles.formTextAreaControl} placeholder="Message" required></textarea>
+                        <div className={styles.buttonContainer}>
+                            <button type="submit" className={styles.button}>Submit</button>
+                        </div>    
                     </form>
                 </div>
             </div>
