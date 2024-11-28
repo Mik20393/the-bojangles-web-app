ce421d049ce3537931820d27d58c3d057c98c07a
Michael Hird <mhird@smartergridsolutions.com>
2024-11-28 09:52:49 +0000

    Updating the padding on the music and about section

--- src/components/aboutSection.js
+++ src/components/aboutSection.js
@@ -5,17 +5,18 @@ export default function AboutSection() {
         <div className={styles.aboutSectionContainer}>
             <div className={styles.aboutSectionTextBox}>
                 <p className={styles.aboutSectionContent}>
-                    We are The Bojangles, an alternative hip-hop live band <br />
-                    from Glasgow, formed in 2023. We love experimenting with <br />
-                    jazzy, funky feel-good rhythyms, our lyrics range from <br />
-                    playful to introspective. <br />
-                    <br />
-                    The band came together through a serious of fortuitous <br />
-                    encounters at a local pub, where musicians from different <br />
-                    backgrounds and musical influences connected, all sharing <br />
-                    a passion and desire to be a part of something exciting and <br />
-                    unique. Our music features Portuguese lyrics, Latin <br />
-                    melodies, boom-bap beats, funky guitar riffs, brass <br />
+                    We are The Bojangles, an alternative hip-hop live band
+                    from Glasgow, formed in 2023. We love experimenting with
+                    jazzy, funky feel-good rhythyms, our lyrics range from
+                    playful to introspective.
+                </p>
+                <p className={styles.aboutSectionContent}>
+                    The band came together through a serious of fortuitous
+                    encounters at a local pub, where musicians from different
+                    backgrounds and musical influences connected, all sharing
+                    a passion and desire to be a part of something exciting and
+                    unique. Our music features Portuguese lyrics, Latin
+                    melodies, boom-bap beats, funky guitar riffs, brass
                     solos - you name it, all played live!            
                 </p>
             </div>
