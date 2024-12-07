0dddfa59d9b8cb47873697491a74166803f4e845
Michael Hird <mhird20@gmail.com>
2024-12-07 11:22:29 +0000

    Adding bandcamp widget

--- src/components/musicSection.js
+++ src/components/musicSection.js
@@ -3,7 +3,12 @@ import styles from "./musicSection.module.css";
 export default function MusicSection() {
     return(
         <div className={styles.spotifyWidgetContainer}>
-            <iframe width= "100%" height= "100%" src="https://bandcamp.com/EmbeddedPlayer/track=2982826395/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/" seamless><a href="https://thebojangles.bandcamp.com/track/tocar-te">Tocar-te by The Bojangles</a></iframe>
+            <iframe 
+                width= "100%" 
+                height= "100%" 
+                src="https://bandcamp.com/EmbeddedPlayer/track=2982826395/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/" seamless>
+                <a href="https://thebojangles.bandcamp.com/track/tocar-te">Tocar-te by The Bojangles</a>
+            </iframe>
         </div>
         
     )
