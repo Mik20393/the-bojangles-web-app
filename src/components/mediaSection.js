db416be6c007dd041c441caa181bc89ffa7b4bc2
Michael Hird <mhird20@gmail.com>
2024-12-17 17:22:05 +0000

    Adding instagram feed

--- src/components/mediaSection.js
+++ src/components/mediaSection.js
@@ -1,32 +1,11 @@
 import styles from "./mediaSection.module.css";
 
-import Instagram from "instagram-web-api";
-
 export default function MediaSection({ posts }) {
     console.log({posts})
     return(
         <div className={styles.mediaSectionContainer}>
-            
+            <iframe src="https://www.juicer.io/api/feeds/bojangles_music/iframe" frameborder="0" width="100%" height="100%"></iframe>
         </div>
+        
     )
-}
-
-export async function getInstagramFeed(context) {
-
-    const username = 'bojangles_music'
-    const password = 'Bojanglesglasgow2024.' 
-    
-    const client = new Instagram({username: username, password: password})
-    
-    await client.login();
-    
-    const response = await client.getPhotosByUsername({
-        username: username,
-    });
-    
-        return {
-            props: {
-                posts: response.user.edge_owner_to_timeline_media.edges,
-            }, // will be passed to the page component as props
-      };
 }
