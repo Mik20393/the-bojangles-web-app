695afbb202b41e5ed2ee7bd5422d4c0794afacc6
Michael Hird <mhird@smartergridsolutions.com>
2024-12-17 16:14:54 +0000

    Adding instagram web api library

--- src/components/mediaSection.js
+++ src/components/mediaSection.js
@@ -1,51 +1,32 @@
-import Image from 'next/image';
-
 import styles from "./mediaSection.module.css";
 
-import mainImage from "@/public/mosh pit.jpg"
-import studioPic from "@/public/guitars.jpg"
-import walter from "@/public/walter.jpg"
-import ja from "@/public/ja.jpg"
+import Instagram from "instagram-web-api";
 
-export default function MediaSection() {
+export default function MediaSection({ posts }) {
+    console.log({posts})
     return(
         <div className={styles.mediaSectionContainer}>
-            <div className={styles.largeImageContentContainer}>
-                <div className={styles.largeImageContainer}>
-                    <Image 
-                        src={mainImage}
-                        alt="Image of The Bojangles performing live at Bloc+"
-                        fill={true}
-                        objectFit="cover"
-                    />
-                </div>
-            </div>
-            <div className={styles.smallImageContentConatainer}>
-                <div className={styles.smallImageContainer}>
-                    <Image 
-                        src={studioPic}
-                        alt="Image of The Bojangles recording at Dystopia Studios"
-                        fill={true}
-                        objectFit="cover"
-                    />
-                </div>
-                <div className={styles.smallImageContainer}>
-                    <Image 
-                        src={walter}
-                        alt="Image of The Bojangles performing live at Bloc+"
-                        fill={true}
-                        objectFit="cover"
-                    />
-                </div>
-                <div className={styles.smallImageContainer}>
-                    <Image 
-                        src={ja}
-                        alt="Image of The Bojangles recording at Dystopia Studios"
-                        fill={true}
-                        objectFit="cover"
-                    />
-                </div>
-            </div>
+            
         </div>
     )
+}
+
+export async function getInstagramFeed(context) {
+
+    const username = 'bojangles_music'
+    const password = 'Bojanglesglasgow2024.' 
+    
+    const client = new Instagram({username: username, password: password})
+    
+    await client.login();
+    
+    const response = await client.getPhotosByUsername({
+        username: username,
+    });
+    
+        return {
+            props: {
+                posts: response.user.edge_owner_to_timeline_media.edges,
+            }, // will be passed to the page component as props
+      };
 }
