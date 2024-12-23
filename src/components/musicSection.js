85ffa48f4f8b3fde369e30f1503175e68a37b0ae
Michael Hird <mhird20@gmail.com>
2024-12-23 11:25:08 +0000

    Update music section height

--- src/components/musicSection.js
+++ src/components/musicSection.js
@@ -2,10 +2,9 @@ import styles from "./musicSection.module.css";
 
 export default function MusicSection() {
     return(
-        <div className={styles.spotifyWidgetContainer}>
+        <div className={styles.bandCampWidgetContainer}>
             <iframe 
-                width= "100%" 
-                height= "100%" 
+                className={styles.bandCampWidget}
                 src="https://bandcamp.com/EmbeddedPlayer/track=2982826395/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/" seamless>
                 <a href="https://thebojangles.bandcamp.com/track/tocar-te">Tocar-te by The Bojangles</a>
             </iframe>
