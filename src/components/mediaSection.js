5e0576c8f159dce82d12ba06b43e9340823e811e
Michael Hird <mhird20@gmail.com>
2024-12-23 11:50:36 +0000

    Update music section height

--- src/components/mediaSection.js
+++ src/components/mediaSection.js
@@ -20,8 +20,8 @@ export default function MediaSection() {
 
     return(
         
-        <div ref={instaFeed} className={styles.mediaSectionContainer}>
-            
+        <div className={styles.mediaSectionContainer}>
+            <iframe src="https://embedsocial.com/api/pro_hashtag/dc30aef1de0771db7d141ab66e6f1a4d8642f827" width="100%" height="100%" frameborder="0"></iframe>
         </div>
         
     )
