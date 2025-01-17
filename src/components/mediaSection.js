2c981dec0dfedd598d8c769fe947a5293b0586d5
Michael Hird <mhird20@gmail.com>
2025-01-17 10:02:50 +0000

    Updating media section styling

--- src/components/mediaSection.js
+++ src/components/mediaSection.js
@@ -1,23 +1,6 @@
-"use client"
-
-import { useEffect , useRef } from "react"
-import Script from "next/script";
 import styles from "./mediaSection.module.css";
 
 export default function MediaSection() {
-    const instaFeed = useRef(null);
-
-    useEffect(() => {
-        if(instaFeed.current) {
-            const script = document.createElement("script");
-            script.src = "https://www.juicer.io/embed/bojangles_music/embed-code.js";
-            script.async = true;
-            instaFeed.current.appendChild(script);
-
-            
-        }
-    }, [])
-
     return(
         
         <div className={styles.mediaSectionContainer}>
