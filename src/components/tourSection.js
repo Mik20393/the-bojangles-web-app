6dc14e5a3adc486ca77ca917b9200fd2d9b7ba5c
Michael Hird <mhird20@gmail.com>
2025-03-24 09:16:33 +0000

    Adding new gig date

--- src/components/tourSection.js
+++ src/components/tourSection.js
@@ -7,25 +7,20 @@ import Slideshow from "./slideshow";
 import gigPoster20250207 from "@/public/gig_poster_20250207 .jpg";
 import gigPoster20250314 from "@/public/gig_poster_20250314.jpg";
 import gigPoster20250321 from "@/public/gig_poster_20250321.jpg";
+import gigPoster20250406 from "@/public/gig_poster_20250406(1).jpg";
 
 export default function TourSection() {
-    const images = [gigPoster20250321, gigPoster20250314, gigPoster20250207];
+    const images = [gigPoster20250406, gigPoster20250321, gigPoster20250314, gigPoster20250207];
     return (
         <>
             <div className={styles.tourSectionContainer}>
                 <div className={styles.liveShowDetailsContainer}>
                     <div className={styles.gigDetailsContainer}>
                         <GigDetails 
-                            venu="BLOC +"
-                            date="14/03/25"
-                            place="117 Bath Street, Glasgow, G2 2SZ"
-                            tickets="FREE ENTRY"
-                        />
-                        <GigDetails 
-                            venu="Room 2"
-                            date="21/03/25"
-                            place="69 Nelson Mandela Place, Glasgow, G2 1QY"
-                            ticketLink="https://ra.co/events/2108643" 
+                            venu="Mono"
+                            date="06/04/25"
+                            place="12 Kings Court, Kings Street, Glasgow, G1 5RB"
+                            ticketLink="https://www.eventbrite.co.uk/e/to-glasgow-with-love-x-fundraiser-for-refuweegee-tickets-1291210267669" 
                         />
                     </div>
                     <div className={styles.posterContentContainer}>
