26ea352bcad81fc1346fd2261db7d79b47b3ba0b
Michael Hird <mhird20@gmail.com>
2025-04-17 14:28:53 +0100

    Mapping products onto product cards

--- src/components/shopPreviewSection.js
+++ src/components/shopPreviewSection.js
@@ -5,9 +5,19 @@ import styles  from './shopPreviewSection.module.css';
 export default function ShopPreview({ products }) {
     return (
         <div className={styles.shopPreviewContainer}>
-            <Image src={products[0].image_url} width="200" height="200"/>
-            <h1>{products[0].name}</h1>
-            <h1>{products[0].price}</h1>
+            {products.map((product) => (
+                <div className={styles.productCard} key={product.id}>
+                    <Image src={product.image_url} width="200" height="200"/>
+                    <h2 className={styles.productDetails}>{product.name}</h2>
+                    <h3 className={styles.productDetails}>£{product.price}.00</h3>
+                    <select className={styles.sizeSelect}>
+                        {product.sizes.map((size) => (
+                            <option key={size} value={size}>{size}</option>
+                        ))}
+                    </select>
+                    <button className={styles.addToBasket}>Add To Basket</button>
+                </div>
+            ))}
         </div>
     )
 }
