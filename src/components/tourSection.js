90b79728f47380c1a15e7f903a9625adfb6e59f4
Michael Hird <mhird20@gmail.com>
2025-05-21 15:12:44 +0100

    Add upcoming gig dates

--- src/components/tourSection.js
+++ src/components/tourSection.js
@@ -12,10 +12,10 @@ export default function TourSection({ posters }) {
                 <div className={styles.liveShowDetailsContainer}>
                     <div className={styles.gigDetailsContainer}>
                         <GigDetails 
-                            venu="Vic Bar"
-                            date="17/05/25"
-                            place="The Art School, 20 Scott St, Glasgow G3 6PE"
-                            ticketLink="https://www.skiddle.com/whats-on/Glasgow/The-Vic-Bar/Belle-poque-VicBar-TelekinephewsSloblinBojanglesMoonsoup/40949492/" 
+                            venu="Rum Shack"
+                            date="13/06/25"
+                            place="657 - 659 Pollokshaws Road, Glasgow, G41 2AB"
+                            ticketLink="https://www.citizenticket.com/events/commotion-promotions/the-bojangles/" 
                         />
                     </div>
                     <div className={styles.posterContentContainer}>
